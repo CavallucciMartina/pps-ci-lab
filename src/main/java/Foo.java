d75bc9fb5d3b91cda5137ef6153c06462c31bc29
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-05-04 14:31:30 +0200

    Add Foo class

---
+++ src/main/java/Foo.java
@@ -0,0 +1,17 @@
+/*
+ * Foo.java
+ * Copyright (C) 2020 Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public class Foo
+{
+    private Foo() { }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Foo");
+    }
+}
+
+// vim: noai:ts=4:sw=4
