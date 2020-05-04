e6f1b82e58a5cdc5da85eb24d77cff054d326427
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2020-05-04 15:33:43 +0200

    Add test class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,28 @@
 plugins {
   id("java")
-  id("application")
+    id("application")
 }
 
 repositories {
   mavenCentral()
 }
 
-dependencies {
+val junitVersion: String = "5.3.2"
 
+dependencies {
+  testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 application {
   mainClassName = "Foo"
-  }
+}
 
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
 
---
+++ src/test/java/FooTest.java
@@ -0,0 +1,19 @@
+/*
+ * FooTest.java
+ * Copyright (C) 2020 Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class FooTest
+{
+    @Test
+    public void FooTest() {
+        assertEquals(true, true);
+    }
+}
+
+// vim: noai:ts=4:sw=4
